b56384d7884606ea6a7a78aebdc6e28bf17e4f9e
Rajeshwarikota <kotarajeswari003.com>
2023-02-25 13:26:56 +0530

    [Rajeshwari] Adding file creation

--- FileOperations/Program.cs
+++ FileOperations/Program.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileOperations
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("...File Operation...");
-            Console.ReadLine();
+            FileCreate();
+           
+        }
+        public static void FileCreate() 
+        {
+            string path = ("C:/Users/LENOVO/source/repos/FileOperations/FileOperations/abc.txt");
+            FileStream obj = new FileStream(path, FileMode.Create);
+          {
+              Console.WriteLine("File is created");
+              obj.Close();
+          }
+             Console.ReadLine();
         }
     }
 }
+
